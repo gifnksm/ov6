340fbdb536eae91886289fd03228f6fc7aafdb05
gifnksm <makoto.nksm+github@gmail.com>
2025-02-27 21:48:54 +0900

    page_alloc: Remove PageBox

--- crates/page_alloc/src/lib.rs
+++ crates/page_alloc/src/lib.rs
@@ -1,10 +1,6 @@
 #![cfg_attr(not(test), no_std)]
 
-pub mod boxed;
 pub mod heap_allocator;
 pub mod page_frame_allocator;
 
-pub use self::{
-    boxed::PageBox,
-    page_frame_allocator::{PageFrameAllocator, RetrievePageFrameAllocator},
-};
+pub use self::page_frame_allocator::{PageFrameAllocator, RetrievePageFrameAllocator};
