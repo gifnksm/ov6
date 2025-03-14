4ba059cf527110f2f2758b79d461c3be72092978
gifnksm <makoto.nksm+github@gmail.com>
2025-03-14 18:40:15 +0900

    initcode: add loop after calling exit

--- user/src/bin/initcode.rs
+++ user/src/bin/initcode.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::hint;
+
 use ov6_syscall::{UserSlice, syscall};
 use ov6_user_lib::os::ov6::syscall::ffi::SyscallExt as _;
 
@@ -11,4 +13,7 @@ extern "C" fn main() {
     let argv = [UserSlice::new(&init)];
     let _ = syscall::Exec::call_raw((UserSlice::new(&init), UserSlice::new(&argv)));
     let _ = syscall::Exit::call_raw((-1,));
+    loop {
+        hint::spin_loop();
+    }
 }
