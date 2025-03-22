6ca0e779b963e9f2ad225797443734b83a6dcfbe
gifnksm <makoto.nksm+github@gmail.com>
2025-03-23 00:39:36 +0900

    sh: Not print prompt when processing commands from file or pipe.
    fixes gifnksm/ov6#47

--- crates/user/ov6_utilities/src/bin/sh/main.rs
+++ crates/user/ov6_utilities/src/bin/sh/main.rs
@@ -5,12 +5,13 @@ extern crate alloc;
 use alloc::string::String;
 use core::mem;
 
+use once_init::OnceInit;
 use ov6_user_lib::{
     eprint,
     error::Ov6Error,
-    fs::File,
-    io::{self},
-    os::fd::AsRawFd as _,
+    fs::{File, StatType},
+    io::{self, STDIN_FD},
+    os::{fd::AsRawFd as _, ov6::syscall},
     process::{self},
 };
 use ov6_utilities::{OrExit as _, exit_err, message_err};
@@ -23,8 +24,12 @@ mod parser;
 mod run;
 mod tokenizer;
 
+static SHOW_PROMPT: OnceInit<bool> = OnceInit::new();
+
 fn get_cmd(buf: &mut String) -> Result<Option<&str>, Ov6Error> {
-    eprint!("$ ");
+    if *SHOW_PROMPT.get() {
+        eprint!("$ ");
+    }
     buf.clear();
     let n = io::stdin().read_line(buf)?;
     if n == 0 {
@@ -42,6 +47,11 @@ fn main() {
         }
         break;
     }
+
+    let isatty = syscall::fstat(STDIN_FD)
+        .ok()
+        .is_some_and(|meta| meta.ty == StatType::Dev as u16);
+    SHOW_PROMPT.init(isatty);
 
     // Read and run input commands.
     let mut buf = String::new();
