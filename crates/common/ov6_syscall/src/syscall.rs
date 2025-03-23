2174636b9216d6471854834741ec35befef40d79
gifnksm <makoto.nksm+github@gmail.com>
2025-03-23 12:00:41 +0900

    syscall: Refactor macro to use more Rust syntax

--- crates/common/ov6_syscall/src/syscall.rs
+++ crates/common/ov6_syscall/src/syscall.rs
@@ -8,48 +8,42 @@ use crate::{
 };
 
 macro_rules! syscall {
-        ($name:ident => fn($arg:ty $(,)?) -> $ret:ty) => {
+    ($( struct $name:ident (fn($($arg:ty),* $(,)?) -> $ret:ty ) ;) *) => {
+        $(
             pub struct $name {}
 
             impl Syscall for $name {
-                type Arg = ( $arg ,);
+                type Arg = ( $($arg ,)* );
                 type Return = $ret;
 
                 const CODE: SyscallCode = SyscallCode::$name;
             }
-        };
-        ($name:ident => fn($($arg:ty),* $(,)?) -> $ret:ty) => {
-            pub struct $name {}
+        )*
+    };
+}
 
-            impl Syscall for $name {
-                type Arg = ( $($arg),* );
-                type Return = $ret;
-
-                const CODE: SyscallCode = SyscallCode::$name;
-            }
-        };
-    }
-
-syscall!(Fork => fn() -> Result<Option<ProcId>, SyscallError>);
-syscall!(Exit => fn(i32) -> Infallible);
-syscall!(Wait => fn(WaitTarget, UserMutRef<i32>) -> Result<ProcId, SyscallError>);
-syscall!(Pipe => fn(UserMutRef<[RawFd; 2]>) -> Result<(), SyscallError>);
-syscall!(Read => fn(RawFd, UserMutSlice<u8>) -> Result<usize, SyscallError>);
-syscall!(Kill => fn(ProcId) -> Result<(), SyscallError>);
-syscall!(Exec => fn(UserSlice<u8>, UserSlice<UserSlice<u8>>) -> Result<Infallible, SyscallError>);
-syscall!(Fstat => fn(RawFd, UserMutRef<Stat>) -> Result<(), SyscallError>);
-syscall!(Chdir => fn(UserSlice<u8>) -> Result<(), SyscallError>);
-syscall!(Dup => fn(RawFd) -> Result<RawFd, SyscallError>);
-syscall!(Getpid => fn() -> ProcId);
-syscall!(Sbrk => fn(isize) -> Result<usize, SyscallError>);
-syscall!(Sleep => fn(Duration) -> Result<(), SyscallError>);
-syscall!(Open => fn(UserSlice<u8>, OpenFlags) -> Result<RawFd, SyscallError>);
-syscall!(Write => fn(RawFd, UserSlice<u8>) -> Result<usize, SyscallError>);
-syscall!(Mknod => fn(UserSlice<u8>, u32, u16) -> Result<(), SyscallError>);
-syscall!(Unlink => fn(UserSlice<u8>) -> Result<(), SyscallError>);
-syscall!(Link => fn(UserSlice<u8>, UserSlice<u8>) -> Result<(), SyscallError>);
-syscall!(Mkdir => fn(UserSlice<u8>) -> Result<(), SyscallError>);
-syscall!(Close => fn(RawFd) -> Result<(), SyscallError>);
-syscall!(Reboot => fn() -> Result<Infallible, SyscallError>);
-syscall!(Halt => fn(u16) -> Result<Infallible, SyscallError>);
-syscall!(Abort => fn(u16) -> Result<Infallible, SyscallError>);
+syscall! {
+    struct Fork(fn() -> Result<Option<ProcId>, SyscallError>);
+    struct Exit(fn(i32) -> Infallible);
+    struct Wait(fn(WaitTarget, UserMutRef<i32>) -> Result<ProcId, SyscallError>);
+    struct Pipe(fn(UserMutRef<[RawFd; 2]>) -> Result<(), SyscallError>);
+    struct Read(fn(RawFd, UserMutSlice<u8>) -> Result<usize, SyscallError>);
+    struct Kill(fn(ProcId) -> Result<(), SyscallError>);
+    struct Exec(fn(UserSlice<u8>, UserSlice<UserSlice<u8>>) -> Result<Infallible, SyscallError>);
+    struct Fstat(fn(RawFd, UserMutRef<Stat>) -> Result<(), SyscallError>);
+    struct Chdir(fn(UserSlice<u8>) -> Result<(), SyscallError>);
+    struct Dup(fn(RawFd) -> Result<RawFd, SyscallError>);
+    struct Getpid(fn() -> ProcId);
+    struct Sbrk(fn(isize) -> Result<usize, SyscallError>);
+    struct Sleep(fn(Duration) -> Result<(), SyscallError>);
+    struct Open(fn(UserSlice<u8>, OpenFlags) -> Result<RawFd, SyscallError>);
+    struct Write(fn(RawFd, UserSlice<u8>) -> Result<usize, SyscallError>);
+    struct Mknod(fn(UserSlice<u8>, u32, u16) -> Result<(), SyscallError>);
+    struct Unlink(fn(UserSlice<u8>) -> Result<(), SyscallError>);
+    struct Link(fn(UserSlice<u8>, UserSlice<u8>) -> Result<(), SyscallError>);
+    struct Mkdir(fn(UserSlice<u8>) -> Result<(), SyscallError>);
+    struct Close(fn(RawFd) -> Result<(), SyscallError>);
+    struct Reboot(fn() -> Result<Infallible, SyscallError>);
+    struct Halt(fn(u16) -> Result<Infallible, SyscallError>);
+    struct Abort(fn(u16) -> Result<Infallible, SyscallError>);
+}
